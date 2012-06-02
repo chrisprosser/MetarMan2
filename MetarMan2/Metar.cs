c37f375bcee6d31887ac9a6cb44be83d53972d02
unknown <cpros_000@harpy-8.(none)>
2012-06-02 13:06:52 -0700

    first set

---
+++ MetarMan2/Metar.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetarMan2
+{
+    class Metar
+    {
+        public Metar(String rawMetar)
+        { }
+
+        private String metar_;
+
+        public String GetRawMetar()
+        {
+            return metar_;
+        }
+    }
+}
---
+++ UnitTestLibrary1/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+using MetarMan2;
+
+namespace UnitTestLibrary1
+{
+    [TestClass]
+    public class MetarTest
+    {
+        [TestMethod] 
+        public void CtorTest()
+        {
+
+
+            Metar metar = new Metar("KROW");
+        }
+    }
+}
